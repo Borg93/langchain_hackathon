a2b1dc4688657b7b6ac596398a541c0b484d0220
Gabriel <gabriel.borg@atkinsglobal.com>
2023-05-10 14:35:04 +0300

    fixed main

--- openapi_main.py
+++ openapi_main.py
@@ -23,4 +23,23 @@ token_config = dotenv_values(".env")
 
 chat = ChatOpenAI(openai_api_key=token_config['OPENAI_API_KEY'], streaming=True, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]), verbose=True, temperature=0)
 
+# Make SystemMessagePromptTemplate
+prompt=PromptTemplate(
+    template="Propose creative ways to incorporate {food_1} and {food_2} in the cuisine of the users choice.",
+    input_variables=["food_1", "food_2"]
+)
+
+system_message_prompt = SystemMessagePromptTemplate(prompt=prompt)
+
+# Make HumanMessagePromptTemplate
+human_template="{text}"
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+
+
+chat_prompt_with_values = chat_prompt.format_prompt(food_1="Bacon", \
+                                                   food_2="Shrimp", \
+                                                   text="I really like food from Germany.")
+
 resp = chat(chat_prompt_with_values.to_messages())
